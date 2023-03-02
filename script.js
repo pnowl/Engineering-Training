92f8155b877fef22e15e8786160aba9aff75353e
pnowl <123702520+pnowl@users.noreply.github.com>
2023-03-02 15:36:02 -0500

    TT-18

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 console.log("Engineering Training");
 const modalButton = document.getElementById("modalButton");
 console.log("modalButton", modalButton);
+
+
+
+modalButton.addEventListener("click", (event) => {
+    console.log("clicked button!")
+    document.getElementById("modalContainer").classList.toggle("hidden")
+});
