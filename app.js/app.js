7f5be95cddda1877c06473a997f0c4e1b846c18f
pnowl <123702520+pnowl@users.noreply.github.com>
2023-08-25 10:06:53 -0400

    TT-136

--- app.js/app.js
+++ app.js/app.js
@@ -1,11 +1,15 @@
+const path = require('path');
 const express = require('express')
 const app = express()
+const router = express.Router();
+
+
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
-
+app.use('/', router);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
